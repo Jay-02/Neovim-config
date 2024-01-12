5698cf6a402582e0186491801d93d2c3c5c6614b
Jay-02 <jawadalamoush02@gmail.com>
2024-01-12 19:59:17 +0300

    neovim custom neovim configuration

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
+
+vim.keymap.set('n','<leader>pv', vim.cmd.Ex)
+
+vim.opt.showcmd = true
+vim.opt.cursorline = true
+vim.opt.laststatus = 2
+
+
+-- spacing stuff
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
